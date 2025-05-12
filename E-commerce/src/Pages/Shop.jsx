a16827810fccda5f97035ebc6a82e8b5dd583407
Parth Tiwari <parthtiwari670@gmail.com>
2025-05-12 18:44:00 +0530

    feat: add footer and new components for shop page, including popular items, offers, new collections, and newsletter

--- E-commerce/src/Pages/Shop.jsx
+++ E-commerce/src/Pages/Shop.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Hero from '../components/Hero/Hero'
+import Popular from '../components/Popular/Popular'
+import Offers from '../components/Offers/Offers'
+import New from '../components/NewCollection/New'
+import News from '../components/NewsLetter/News'
 
 const Shop = () => {
   return (
     <div>
       <Hero/>
+      <Popular/>
+      <Offers/>
+      <New/>
+      <News/>
     </div>
   )
 }
